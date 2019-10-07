982f0f19382af16557bfbeea323ba7507d768ba9
Aly Nguyen <alynguyen@Alys-MacBook-Pro-2.local>
2019-10-07 00:32:15 -0700

    style bulma cards

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -3,24 +3,29 @@ import SearchBar from '../../components/SearchBar/SearchBar';
 import Navbar from '../../components/Navbar/Navbar';
 import BusinessCard from '../../components/BusinessCard/BusinessCard';
 import 'react-bulma-components/dist/react-bulma-components.min.css';
-// import { Navbar } from 'react-bulma-components';
+import { Columns } from 'react-bulma-components';
+import './MainPage.css'
 
 const MainPage = (props) => {
   return (
-    <div>
+    <div className="container">
       <Navbar 
         handleLogout={props.handleLogout}
         user={props.user}
-      />
+        />
       <SearchBar 
         handleSearch={props.handleSearch}
-      />
+        />
+      <Columns>
       {props.results.map((results, idx) => (
         <BusinessCard
-          key={idx}
-          name={results.name}
+        key={idx}
+        name={results.name}
+        image={results.image_url}
+        location={results.location.display_address[0]}
         />
-      ))}
+        ))}
+      </Columns>
     </div> 
   );
 }
